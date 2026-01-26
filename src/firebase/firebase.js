bd82a421b0a251090af4bbf922bd2c7e89a8ff58
Septian Pratama <pratacips@gmail.com>
2026-01-26 14:07:56 +0700

    update firebase

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,6 +1,6 @@
-// src/firebase.js
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
@@ -12,4 +12,7 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+
+// Ekspor db untuk database dan auth untuk login
 export const db = getFirestore(app);
+export const auth = getAuth(app);
